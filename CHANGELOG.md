de0b6ea52da5b8c7180aad59cf1e250c93b677a7
Alex Scarborough <alex@teak.io>
2022-05-04 15:09:39 -0700

    v0.0.2.

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+## 0.0.2
+
+ENHANCEMENTS:
+
+* Retrieve stats immediately on start.
+
+## 0.0.1
+
+Initial release.
--- lib/fluent/plugin/zookeeper/mntr/version.rb
+++ lib/fluent/plugin/zookeeper/mntr/version.rb
@@ -18,7 +18,7 @@ module Fluent
   module Plugin
     module Zookeeper
       module Mntr
-        VERSION = "0.0.1"
+        VERSION = "0.0.2"
       end
     end
   end
